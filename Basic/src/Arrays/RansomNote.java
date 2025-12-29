5e03cbc1cb5481cf53d74cf08e6f01107eb3681e
ForMyke <golomian16@hotmail.com>
2025-12-29 00:17:26 -0600

    RansomNote

---
+++ Basic/src/Arrays/RansomNote.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.*;
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> counts = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            int currentCount = counts.getOrDefault(c, 0);
+
+            if (currentCount == 0) {
+                return false;
+            }
+            counts.put(c, currentCount - 1);
+        }
+
+        return true;
+    }
+}
